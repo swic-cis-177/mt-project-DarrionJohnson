b2e3764442735b58f286197915e310c26f5aeb66
Manav Misra <manavm@visionwebsoft.com>
2020-12-04 11:13:07 -0600

    Set up TODOs

---
+++ lib.js
@@ -0,0 +1,6 @@
+// TODO{darrion}: 'export' function to 'addWalk'
+
+// Elements are the inputs and values from the form
+export const addWalk = (elements) => {
+  // TODO{darrion}: Take the elements and create a new walk
+};
---
+++ scripts.js
@@ -0,0 +1,5 @@
+import { addWalk } from "./lib.js";
+
+// TODO{darrion}: Import the necessary fxns. such as 'addWalk' and 'renderTable'
+addWalk;
+// TODO{darrion}: Grab the form and attach a 'submit' 'event' handler
