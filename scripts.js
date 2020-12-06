ca0ac23699b2eb8eefc654adbdba9cc154e54ea5
Darrion Johnson <Darrion.Johnson@student.swic.edu>
2020-12-06 05:27:45 -0600

    Shorten the code for creating the table by removing render and have ctreateWalksTabe be called directly from the event handler.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,11 @@ import { addWalk, createWalksTable } from "./lib.js";
 
 const walksTable = [];
 
-const render = (walksData) => {
-  createWalksTable(walksTable);
-};
-
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   walksTable.push(addWalk(event.target.elements));
-  render(walksTable);
+  createWalksTable(walksTable);
 });
